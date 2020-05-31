b1b1cb9e87b3a45e8244f5b7294a4fc68742f16a
kefhifi <1874570622@qq.com>
2020-06-01 01:16:21 +0800

    add

---
+++ socket_study/TCP/tcp_client.py
@@ -0,0 +1,18 @@
+import socket
+
+
+def main():
+    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_socket.connect(("119.45.0.4", 39318))
+    while True:
+        send_data = input("Please input message: ")
+        if send_data != "quit":
+            tcp_socket.send(send_data.encode("utf-8"))
+            recv_data = tcp_socket.recv(1024)
+            print("From Server: " + recv_data[1][0] + ":" + str(recv_data[1][1]) + ":  " recv_data[0].decode("utf-8"))
+        else:
+            break
+    tcp_socket.close()
+
+if __name__ == '__main__':
+    main()
---
+++ socket_study/TCP/tcp_server.py
@@ -0,0 +1,19 @@
+import socket
+
+
+def main():
+    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_socket.bind(("", 39318))  # 绑定
+    tcp_socket.listen(128) # 使用fsocket创建的套接字默认是主动的，使用listen将其变为被动的，这样就可以接收别人的连接了
+    new_socket, client_addr = tcp_socket.accept()  # 等待客户端连接
+    print("waiting client to connet ......")
+    print(client_addr)
+    recv_data = new_socket.recv(1024)
+    print(recv_data)
+    new_socket.send("Hi".encode("utf-8"))
+    new_socket.close()
+    tcp_socket.close()
+
+
+if __name__ == '__main__':
+    main()
