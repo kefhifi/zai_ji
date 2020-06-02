6062cddf2f41a6015431e5509a407be23d8497ef
kefhifi <1874570622@qq.com>
2020-06-02 23:55:29 +0800

    add

--- socket_study/TCP/tcp_server.py
+++ socket_study/TCP/tcp_server.py
@@ -4,10 +4,13 @@ import socket
 def main():
     tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     tcp_socket.bind(("", 39318))  # 绑定
-    tcp_socket.listen(128) # 使用fsocket创建的套接字默认是主动的，使用listen将其变为被动的，这样就可以接收别人的连接了
+    tcp_socket.listen(128) # 使用socket创建的套接字默认是主动的，使用listen将其变为被动的，这样就可以接收别人的连接了
     new_socket, client_addr = tcp_socket.accept()  # 等待客户端连接
-#    print( tcp_socket.accept())  # 等待客户端连接
-# (<socket.socket fd=4, family=AddressFamily.AF_INET, type=SocketKind.SOCK_STREAM, proto=0, laddr=('10.0.0.6', 39318), raddr=('222.131.153.176', 4053)>, ('222.131.153.176', 4053))
+    #print( tcp_socket.accept())  # 等待客户端连接
+    """
+    (<socket.socket fd=4, family=AddressFamily.AF_INET, type=SocketKind.SOCK_STREAM, proto=0, laddr=('10.0.0.6', 39318), 
+    raddr=('222.131.153.176', 4053)>, ('222.131.153.176', 4053))
+    """
     print("waiting client to connet ......")
     print(client_addr)
     recv_data = new_socket.recv(1024)
