b9115149f12ce99e245ce0d39f40d541f7ea941e
kefhifi <1874570622@qq.com>
2020-06-03 00:40:39 +0800

    add

--- socket_study/TCP/tcp_server.py
+++ socket_study/TCP/tcp_server.py
@@ -5,23 +5,20 @@ def main():
     tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     tcp_socket.bind(("", 39318))  # 绑定
     tcp_socket.listen(128) # 使用socket创建的套接字默认是主动的，使用listen将其变为被动的，这样就可以接收别人的连接了
-    new_socket, client_addr = tcp_socket.accept()  # 等待客户端连接
-    #print( tcp_socket.accept())  # 等待客户端连接
-    """
-    (<socket.socket fd=4, family=AddressFamily.AF_INET, type=SocketKind.SOCK_STREAM, proto=0, laddr=('10.0.0.6', 39318), 
-    raddr=('222.131.153.176', 4053)>, ('222.131.153.176', 4053))
-    """
-    print("waiting client to connet ......")
-    print(client_addr)
     while True:
+        new_socket, client_addr = tcp_socket.accept()  # 等待客户端连接
+        #print( tcp_socket.accept())  # 等待客户端连接
+        """
+        (<socket.socket fd=4, family=AddressFamily.AF_INET, type=SocketKind.SOCK_STREAM, proto=0, laddr=('10.0.0.6', 39318), 
+        raddr=('222.131.153.176', 4053)>, ('222.131.153.176', 4053))
+        """
+        print("waiting client to connet ......")
+        print(client_addr)
         recv_data = new_socket.recv(1024)
         recv_data = recv_data.decode("utf-8")
         print(recv_data)
-        if recv_data == "quit":
-            break
-        send_data = input("Input info: ")
-        new_socket.send(send_data.encode("utf-8"))
-    new_socket.close()
+        new_socket.send("Hi".encode("utf-8"))
+        new_socket.close()
     tcp_socket.close()
 
 
