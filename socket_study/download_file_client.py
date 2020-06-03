7f978848f3a58c3ef20821eb01c537a125cc76ce
kefhifi <1874570622@qq.com>
2020-06-03 23:40:19 +0800

    add

---
+++ socket_study/download_file_client.py
@@ -0,0 +1,24 @@
+import socket
+
+def main():
+    download_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    addr = input("inpu server ip: ")
+    port = input("input server port: ")
+    if ! addr:
+        addr = "119.45.0.4"
+    if ! port:
+        port = 39318
+    else:
+        port = int(port)
+    addr_port = (addr, port)
+    download_socket.connect(addr_port)
+    file_name = input("input file name: ")
+    download_socket.send(file_name.encode("utf-8"))
+    recv_data = download_socket.recv(1024*1024)
+    if recv_data:
+        with open("test.data", "wb") as file_obj:
+            file_obj.write(recv_data)
+    download_socket.close()
+
+if __name__ == "__main__":
+    main()
