6e8531f752bf5f53adc8b6ecab43a34c9cc6a84a
kefhifi <1874570622@qq.com>
2020-06-03 23:51:21 +0800

    add

--- socket_study/download_file_server.py
+++ socket_study/download_file_server.py
@@ -6,7 +6,7 @@ server_socket.listen(128)
 while True:
     new_socket, client_addr = server_socket.accept()
     file_name = new_socket.recv(1024)
-    print("Client (%s) download file: %s " %  (str(client_addr), file_name.decode("utf-8"))
+    print("Client (%s) download file: %s " %  (str(client_addr), file_name.decode("utf-8")))
     # with 这种方式适合能打开的情况(万一文件不存在，就打不开了)，比如以w的模式打开。如果以读的模式打开就用open
     try:
         file_obj = open(file_name,"rb")
