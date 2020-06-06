1eeec54c54c923de1837361aa01a412f5f2ed538
kefhifi <1874570622@qq.com>
2020-06-06 22:30:48 +0800

    add

---
+++ thread_process/udp_multithread.py
@@ -0,0 +1,31 @@
+import socket
+import threading
+
+ip_port = ("119.45.0.4", 39318)
+
+def recv_msg(udp_socket):
+    while True:
+        recv_data = udp_socket.recvfrom(1024)
+        print(recv_data[0].decode("utf-8"))
+
+
+
+def send_msg(udp_socket, ip_port):
+    while True:
+        msg = input("input info: ")
+        udp_socket.sendto(msg.encode("utf-8"), ip_port)
+
+
+
+
+def main():
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_socket.bind(("", 39318))
+    t1 = threading.Thread(target=recv_msg, args=(udp_socket,))
+    t2 = threading.Thread(target=send_msg, args=(udp_socket,ip_port))
+    t1.start()
+    t2.start()
+
+
+if __name__ == "__main__":
+    main()
