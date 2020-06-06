fac56738facab70c845859a87bd6729e5b77a2ed
kefhifi <1874570622@qq.com>
2020-06-07 02:35:05 +0800

    add

---
+++ thread_process/process/process_1st.py
@@ -0,0 +1,22 @@
+
+import multiprocessing
+
+
+def test1():
+    while True:
+        print("------1-----")
+
+def test2():
+    while True:
+        print("------2-----")
+
+
+def main():
+    p1 = multiprocessing.Process(target=test1)
+    p2 = multiprocessing.Process(target=test2)
+    p1.start()
+    p2.start()
+
+
+if __name__ == "__main__":
+    main()
