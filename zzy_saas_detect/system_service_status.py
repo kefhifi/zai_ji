276ee3613d0af574e5b7ff609d420fcb3d2e2a60
kefhifi <1874570622@qq.com>
2020-07-06 01:23:22 +0800

    add

---
+++ zzy_saas_detect/system_service_status.py
@@ -0,0 +1,46 @@
+import redis
+import pymysql
+from urllib import request
+
+
+class SystemStatus():
+    def __init__(self, url, name, db_host, db_port, db_password, db_database, redis_host, redis_port, redis_password):
+        self.__status = -1
+        self.__url = url
+        self.__name = name
+        self.__db_host = db_host
+        self.__db_port = db_port
+        self.__db_password = db_password
+        self.__db_database = db_database
+        self.__redis_host = redis_host
+        self.__redis_port = redis_port
+        self.__redis_password = redis_password
+
+    def __status_detect(self):
+        try:
+            result = request.urlopen(self.__url, timeout=5)
+        except Exception as ret:
+            print(ret)
+            self.__status = 0
+        else:
+            # print(type(result.status), result.reason)  # 打印 响应头的状态码、状态
+            # result = result.read()
+            # result = str(result, encoding="utf-8")
+            # print(result)
+            if result.status == 200:
+                self.__status = 1
+
+    def go(self):
+        self.__status_detect()
+
+
+
+
+
+def main():
+    service = SystemStatus("https://semm.zhizhangyi.com:3000/emm", "SEMM", "127.0.0.1", 3306, "123456", "system_status",
+                           "127.0.0.1", 39328, "123456")
+    service.go()
+
+if __name__ == "__main__":
+    main()
